9250d643579dff000aa9e058be45417ff36550ea
Ziv Zamechek <ziv921@yahoo.com>
2016-04-15 15:32:15 -0400

    added links to show and edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require './config/environment'
+
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -7,5 +10,75 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "alpha blog secret"
   end
 
-
+  get '/' do 
+    erb :index
+  end
+  
+  get '/signup' do 
+    if logged_in? 
+      redirect '/articles'
+    else
+      erb :'users/create_user'
+    end
+  end
+  
+  post '/signup' do 
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save && valid_user?(user)
+      session[:user_id] = user.id
+      redirect '/articles'
+    else
+      redirect '/signup'
+    end
+  end
+  
+  get '/login' do 
+    if logged_in? 
+      redirect '/articles'
+    else
+      erb :'/users/login'
+    end
+  end
+  
+  post '/login' do 
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/articles'
+    else
+      redirect '/login'
+    end
+  end
+  
+  get '/logout' do 
+    if logged_in? 
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+      
+  
+  helpers do 
+    def logged_in?
+      !!session[:user_id]
+    end
+    
+    def current_user
+      User.find(session[:user_id])
+    end
+    
+    def auth_redirect(path)
+      logged_in? ? (redirect path ) : (redirect '/login')
+    end
+    
+    def auth_erb(path)
+      logged_in? ? (erb :"#{path}") : (redirect '/login')
+    end
+    
+    def valid_user?(user)
+      user.username.length > 0 && user.email.length > 0 && user.password.length > 0
+    end
+  end
 end
