0a6af679d175d2889fb2b08f2b063ffffb948577
Ziv Zamechek <ziv921@yahoo.com>
2016-04-15 14:43:24 -0400

    added edit form show form and new page form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
 
-  set :views, Proc.new { File.join(root, "../views/") }
-  register Sinatra::Twitter::Bootstrap::Assets
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "alpha blog secret"
+  end
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,13 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 3, maximum: 50 }
   validates :description, presence: true, length: {minimum: 10, maximum: 300 }
 
+  belongs_to :author
+
+  def slug
+    self.title.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |i| i.slug == slug }
+  end
 end
