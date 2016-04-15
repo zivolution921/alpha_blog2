fc0a30e611a0c6bc21bf9ac1e62eba31e2731dca
Ziv Zamechek <ziv921@yahoo.com>
2016-04-15 12:08:14 -0400

    added some files and organized the project

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,12 @@
-environment.rb
+require 'bundler/setup'
+Bundler.require
+
+ENV['SINATRA_ENV'] ||= "development"
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
